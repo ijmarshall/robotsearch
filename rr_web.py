5a430d9d22966119fc06461f57a9d275ca3417ab
Iain Marshall <mail@ijmarshall.com>
2018-06-12 21:24:51 +0100

    amaazing cool webpage of dreams

--- rr_web.py
+++ rr_web.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 # @TODO (?) consider refactoring
-rct_clf = rct_robot.RCTRobot()
-from robotsearch.robots import rct_robot
+
+# from robotsearch.robots import rct_robot
+# rct_clf = rct_robot.RCTRobot()
 
 @app.route("/")
 def hello():
@@ -12,6 +15,12 @@ def hello():
 @app.route("/upload_RIS")
 def upload_RIS():
     pass 
+
+
+@app.route('/start_page')
+def start_page():
+    return render_template('select_ris_to_upload.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

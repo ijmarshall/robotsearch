47ae709345e848b231da83c061f748640d6be6a2
bwallace <byron.wallace@gmail.com>
2018-06-12 16:20:36 -0400

    enormous progress

--- rr_web.py
+++ rr_web.py
@@ -1,12 +1,17 @@
 from flask import Flask
-
 app = Flask(__name__)
 
+# @TODO (?) consider refactoring
+rct_clf = rct_robot.RCTRobot()
+from robotsearch.robots import rct_robot
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/upload_RIS")
+def upload_RIS():
+    pass 
 
 if __name__ == '__main__':
     app.run(debug=True)

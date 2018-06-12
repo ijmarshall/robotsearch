ae2ed963c003b57f8e1c51221896233c5a7e2b36
bwallace <byron.wallace@gmail.com>
2018-06-12 17:21:37 -0400

    uploads fun

--- rr_web.py
+++ rr_web.py
@@ -1,26 +1,56 @@
-from flask import Flask
+import os 
+
+from flask import Flask, request, redirect, url_for
 from flask import render_template
 
+from werkzeug.utils import secure_filename
+
+UPLOAD_DIR_NAME = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'ris'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = resource_path = os.path.join(app.root_path, UPLOAD_DIR_NAME) 
 
 # @TODO (?) consider refactoring
 
 # from robotsearch.robots import rct_robot
 # rct_clf = rct_robot.RCTRobot()
 
-@app.route("/")
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    #return "Hello World!"
+    if request.method == 'POST':
+        import pdb; pdb.set_trace()
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_RIS',
+                                    filename=filename))
+
     return render_template("select_ris_to_upload.html")
 
-@app.route("/upload_RIS")
-def upload_RIS():
-    pass 
+methods=['GET', 'POST']
+@app.route("/uploaded_RIS")
+def uploaded_RIS():
+    import pdb; pdb.set_trace()
 
 
-@app.route('/start_page')
-def start_page():
-    return render_template('select_ris_to_upload.html')
+#@app.route('/start_page')
+#def start_page():
+#    return render_template('select_ris_to_upload.html')
 
 
 if __name__ == '__main__':

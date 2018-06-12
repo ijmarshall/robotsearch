35f58916dd7b0633689f2e14f392747b9e7c902b
bwallace <byron.wallace@gmail.com>
2018-06-12 16:46:41 -0400

    mega progress

--- rr_web.py
+++ rr_web.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    #return "Hello World!"
+    return render_template("select_ris_to_upload.html")
 
 @app.route("/upload_RIS")
 def upload_RIS():
